ba8ef796f56337fbd4bd0fc0257560f729d13963
slipanchor578 <slipanchor578@gmail.com>
2026-07-05 22:52:35 +0900

    :Git commit が成功するように修正 treesitterを有効にしている状態で:Git commit に入るとFileTypeがgitcommitになり treesitterのパーサーとかをDLしてbuildしだす なぜか、これが終わらずにnvimがそのまま落ちるのでFileType == gitcommitの時は treesitter.stop させて回避する

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,6 +1,15 @@
+local createAutoCmd = require("config.util").createAutoCmd
+
 require("tree-sitter-manager").setup({
-  auto_install = true,
+  auto_install = false,
   noauto_install = {
     "c", "lua", "markdown", "markdown_inline", "query", "vim", "vimdoc"
-  }
+  },
+
+  createAutoCmd("FileType", {
+    pattern = { "gitcommit" },
+    callback = function(args)
+      vim.treesitter.stop(args.buf)
+    end,
+  })
 })
--- lua/minis/git.lua
+++ lua/minis/git.lua
@@ -3,7 +3,7 @@ local safely = require("mini.misc").safely
 safely("later", function()
   -- これ無しで「:Git commit」とかすると、nanoとかを裏で開いてしまう
   -- で、gitは入力待ちし、mini.gitもgitの処理が終わるまで待つのでdeadlockしnvimが落ちる
-  -- これをすると、nvimを開いている時は、git commit に使うエディタを今開いているnvimにしてくれるので落ちない?
+  -- これをすると、nvimを開いている時は、git commit に使うエディタを今開いているnvimにしてくれるので落ちない
   if vim.v.servername and vim.v.servername ~= "" then
     vim.env.GIT_EDITOR = "nvim --server " .. vim.v.servername .. " --remote-tab-wait"
   end
